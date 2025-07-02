20bfc221d4f4ceb1d3f27573847070a88932ac79
narendnp <kadektop2@gmail.com>
2025-07-02 17:16:30 +0800

    fix python path retrieval

--- run-rembg-service.ps1
+++ run-rembg-service.ps1
@@ -6,14 +6,24 @@ $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 # Change to the script directory
 Set-Location $scriptPath
 
-# Get Python executable path from the registry
-$pythonPath = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Python\PythonCore\*\InstallPath' -ErrorAction SilentlyContinue).ExecutablePath
+# Try to find Python in path
+$pythonPath = Get-Command python -ErrorAction SilentlyContinue
+
 if (-not $pythonPath) {
-    $pythonPath = (Get-ItemProperty -Path 'HKCU:\SOFTWARE\Python\PythonCore\*\InstallPath' -ErrorAction SilentlyContinue).ExecutablePath
+    # Get Python executable path from the registry
+    $pythonPath = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Python\PythonCore\*\InstallPath' -ErrorAction SilentlyContinue).ExecutablePath
+    if (-not $pythonPath) {
+        $pythonPath = (Get-ItemProperty -Path 'HKCU:\SOFTWARE\Python\PythonCore\*\InstallPath' -ErrorAction SilentlyContinue).ExecutablePath
+    }
+}
+
+if (-not $pythonPath) {
+    Write-Error "Python is not found. Please install python and add it to PATH"
+    exit 1
 }
 
 # Replace python.exe with pythonw.exe for no-console execution
-$pythonwPath = $pythonPath -replace "python\.exe$", "pythonw.exe"
+$pythonwPath = $pythonPath.Source -replace "python\.exe$", "pythonw.exe"
 
 # Start the Flask app
 & $pythonwPath "app.py"
